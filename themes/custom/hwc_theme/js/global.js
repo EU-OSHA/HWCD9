9692a5b36f4ccc540ca8b6707ec9ab52440b47d7
ilorenzo10 <jilorenzo@bilbomatica.es>
2022-07-18 08:32:55 +0200

    all site css organisation

--- themes/custom/hwc_theme/js/global.js
+++ themes/custom/hwc_theme/js/global.js
@@ -58,37 +58,54 @@ jQuery(function ($) {
     });
 
     //menu toolkit
-    $("#block-campaigntoolkit > .menu >.menu-item.expanded > span").click(function(){
-      if($(this).siblings("ul").is(":visible")){
-        $(this).removeClass("up-arrow");
-        $(this).addClass("closed-down-arrow");
-        $('#block-campaigntoolkit').removeClass("opened");
-        $(this).siblings("ul").slideUp();
+
+    function OpenMenu(element, menu){
+      if($(element).siblings("ul").is(":visible")){
+        $(element).removeClass("up-arrow");
+        $(element).addClass("closed-down-arrow");
+        $(menu).removeClass("opened");
+        $(element).siblings("ul").slideUp();
       }
       else {
-        $(this).addClass("up-arrow");
-        $(this).removeClass("closed-down-arrow");
-        $('#block-campaigntoolkit').addClass("opened");
-        $(this).siblings("ul").slideDown();
+        $(element).addClass("up-arrow");
+        $(element).removeClass("closed-down-arrow");
+        $(menu).addClass("opened");
+        $(element).siblings("ul").slideDown();
       }
+    }
+    function OpenMenuLevel1(element){
+      if( $(element).hasClass( "active" ) ) {
+        $(element).parent().css('display','block');
+      }
+    }
+    function OpenMenuLevel2(element){
+      if( $(element).hasClass( "active" ) ) {
+        $(element).parent().parent().css('display','block');
+      }
+    }
+
+    $("#block-campaigntoolkit > .menu >.menu-item.expanded > span").click(function(){
+      OpenMenu( $(this), '#block-campaigntoolkit' )
+    });
+    $("#block-campaigntoolkit-2 > .menu >.menu-item.expanded > span").click(function(){
+      OpenMenu( $(this), '#block-campaigntoolkit-2' )
     });
 
     $('#block-campaigntoolkit .submenu-1 > li').each(function( index ) {
-      if( $(this).hasClass( "active" ) ) {
-        $(this).parent().css('display','block');
-      }
+      OpenMenuLevel1( $(this) )
+    });
+    $('#block-campaigntoolkit-2 .submenu-1 > li').each(function( index ) {
+      OpenMenuLevel1( $(this) )
     });
 
     $('#block-campaigntoolkit .submenu-2 > li').each(function( index ) {
-      if( $(this).hasClass( "active" ) ) {
-        $(this).parent().parent().css('display','block');
-      }
+      OpenMenuLevel2( $(this) )
+    });
+    $('#block-campaigntoolkit-2 .submenu-2 > li').each(function( index ) {
+      OpenMenuLevel2( $(this) )
     });
 
     /* sharelink in napo films */
-
-
-
     $('.share-link').on( "click", function() {
       console.log($(this));
       var wrapper = $(this).parent().parent();
@@ -100,12 +117,19 @@ jQuery(function ($) {
       $(this).parent().toggleClass('d-none');
     });
 
+    /* hide sharelinks when the user clicks out */
     $(document).mouseup(function(e) {
       var container = $(".list-networks");
       // if the target of the click isn't the container nor a descendant of the container
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         container.addClass('d-none');
       }
+    });
+
+    /* landing pages grid: go to specefic url when the user clicks into the item div  */
+    $('.landing-menu-item').on( "click", function() {
+        var urlLink = $('>a',this).attr('href');
+        window.location.href= urlLink;
     });
 
 
